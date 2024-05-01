92a6ea37addbde969dd77e5c858e7090650f1ef1
mariadev2 <pmariaeduarda987@gmail.com>
2024-04-30 21:35:33 -0300

    tela login em andamneto

--- sucum-web/src/App.js
+++ sucum-web/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <body>
+        <div class="container">
+        
+            <div class="center"> 
+            <div class= "circulo"></div>
+            <label class="Entrar">Entrar</label>
+            <label class="Seja">Seja bem vindo(a)!</label>
+
+
+            <div class="input-container">
+                  <input type="text" id="cpf" name="cpf"/>
+                  <input type="text" id="senha" name="senha"/>
+            </div>
+            <button class= "botao">Entrar</button>
+          </div>
+          </div>
+      </body>
+
   );
 }
 
