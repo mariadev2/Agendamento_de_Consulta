33bb39af6ff4a54b145c212361fafd105e5082ce
Andrei dos Santos Moreira <andrei.moreira@solutis.com.br>
2024-05-22 23:29:30 -0300

    fix: remove unnecessary logs

--- sucum-api/src/api/controllers/userController.js
+++ sucum-api/src/api/controllers/userController.js
@@ -24,7 +24,6 @@ export default () => {
             getInstanceDB.query(queryLogin, (err, data)=>{
                 if (err) res.status(500).json({messageError: 'Error sql: ' + err.sqlMessage});
                 const result = Object.values(JSON.parse(JSON.stringify(data)));
-                console.log(result);
                 if (result.length > 0) {
                     result.forEach(element => {
                         if (element.username === username && element.senha === senha) {
